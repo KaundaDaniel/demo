7d1479f3fd93bf90ebff11bd342506444fab0f67
KaundaDaniel <dannydagmaisutic@gmail.com>
2023-07-17 23:13:27 +0100

    Controller

---
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.entities.Client;
+import com.example.demo.service.ClientServe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/")
+public class ClientController {
+    @Autowired
+    private ClientServe clientServie;
+    @GetMapping
+    public ResponseEntity<Client> mostra() throws UnknownHostException {
+        return ResponseEntity.status(HttpStatus.OK).body(clientServie.clientIp());
+    }
+
+}
